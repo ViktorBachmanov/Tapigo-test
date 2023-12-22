44bc39c62c71d2869d339a0c6018ea271f7c8e61
Viktor Bachmanov <vbachmanov@mail.ru>
2023-12-22 14:56:48 +0300

    Install NuxtUI Library

--- src/nuxt/app.vue
+++ src/nuxt/app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- <NuxtWelcome /> -->
+
+  <UContainer>
+    <ColorModeButton />
+  </UContainer>
+
+  <NuxtPage />
 </template>
---
+++ src/nuxt/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>Hello, Tapigo!</h3>
+</template>
